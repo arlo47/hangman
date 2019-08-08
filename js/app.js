50672609ca6f19241917e49611e09abeb29df0d5
contentisking <gfitzpatrick2@gmail.com>
2019-08-08 16:06:27 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,19 +1,31 @@
+/**NOTES
+ * having both chancesLeft and hangIndex is kind of redundant.
+ * having my functions directly modify global variables does not follow functional programming methodology
+ * 1 off problem on line 53 (view.drawHangMan())
+ */
+
 const firstAnswer = ["I", " ", "l", "o", "v", "e", " ", "J", "a", "v", "a", "S", "c", "r", "i", "p", "t"];
+const alphabet = document.querySelectorAll("figure");
 let userAnswer = [];
 let chancesLeft = 6;
-
-
+let hangIndex = 0;
 
 let game = {
-    userGuess: function() {
-        game.getAnswerLength();
-        let letter = prompt("Guess a letter:");
-    
+    userGuess: function(e) {
+        let letter = e.target.innerText;
+        let wasWrong = true;
+
         for(let i = 0; i < firstAnswer.length; i++) {
-            if(letter === firstAnswer[i]) {
-                userAnswer.splice(i, 0, letter);
+            if(letter.toLowerCase() === firstAnswer[i].toLowerCase()) {
+                userAnswer.splice(i, 1, letter);
+                wasWrong = false;
             }
         }
+        if(wasWrong) {
+            console.log("Wrong! " + --chancesLeft + " changes left.");
+            view.drawHangMan();
+        }
+        view.outputAnswer();
     },
     getAnswerLength: function() {
         for(let i = 0; i < firstAnswer.length; i++) {
@@ -31,8 +43,21 @@ let view = {
     outputAnswer: function() {
         let answerContainer = document.getElementById("answer-container");
 
-        game.getAnswerLength();
+        answerContainer.innerHTML = "";
         answerContainer.innerHTML = userAnswer.join("");
+    },
+    drawHangMan: function() {
+        let hangman = document.getElementById("hangman");
+        let hangmanArr = ["hangman-0.jpg", "hangman-1.jpg", "hangman-2.jpg", "hangman-3.jpg", "hangman-4.jpg", "hangman-5.jpg", "hangman-6.jpg"];
+        
+        hangIndex < 6 ? hangIndex++ : view.gameOver();
+        hangman.src = "img/" + hangmanArr[hangIndex];
+    },
+    gameOver: function() {
+        let gameOverContainer = document.getElementById("game-over");
+        gameOverContainer.classList.toggle("hide");
     }
 }
 
+game.getAnswerLength();
+alphabet.forEach((letter) => letter.addEventListener("click", game.userGuess));
