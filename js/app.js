114e1d21711389f76a2e5995c1cabdd6cbdb536a
contentisking <gfitzpatrick2@gmail.com>
2019-08-08 16:07:05 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,62 +2,67 @@
  * having both chancesLeft and hangIndex is kind of redundant.
  * having my functions directly modify global variables does not follow functional programming methodology
  * 1 off problem on line 53 (view.drawHangMan())
+ * Disable/fade out letters once they are picked
  */
 
-const firstAnswer = ["I", " ", "l", "o", "v", "e", " ", "J", "a", "v", "a", "S", "c", "r", "i", "p", "t"];
-const alphabet = document.querySelectorAll("figure");
-let userAnswer = [];
-let chancesLeft = 6;
-let hangIndex = 0;
-
 let game = {
+    firstAnswer: "I love JavaScript".split(""),
+    userAnswer: [],
+    alphabet: document.querySelectorAll("figure"),
+    chancesLeft: 6,
     userGuess: function(e) {
         let letter = e.target.innerText;
         let wasWrong = true;
 
-        for(let i = 0; i < firstAnswer.length; i++) {
-            if(letter.toLowerCase() === firstAnswer[i].toLowerCase()) {
-                userAnswer.splice(i, 1, letter);
+        for(let i = 0; i < game.firstAnswer.length; i++) {
+            if(letter.toLowerCase() === game.firstAnswer[i].toLowerCase()) {
+                game.userAnswer.splice(i, 1, letter);
                 wasWrong = false;
             }
         }
         if(wasWrong) {
-            console.log("Wrong! " + --chancesLeft + " changes left.");
+            console.log("Wrong! " + --game.chancesLeft + " changes left.");
             view.drawHangMan();
         }
         view.outputAnswer();
     },
     getAnswerLength: function() {
-        for(let i = 0; i < firstAnswer.length; i++) {
-            firstAnswer[i] === " " ? userAnswer.push(" ") : userAnswer.push("_");
+        for(let i = 0; i < game.firstAnswer.length; i++) {
+            game.firstAnswer[i] === " " ? game.userAnswer.push(" ") : game.userAnswer.push("_");
         }
     }
 }
 
 let view = {
-    toString: function() {
-        console.log("Guess:  " + userAnswer.join(""));
-        console.log("Answer: " + firstAnswer.join(""));
-        console.log("Lives:  " + chancesLeft);
-    },
+    hangIndex: 0,
     outputAnswer: function() {
         let answerContainer = document.getElementById("answer-container");
 
         answerContainer.innerHTML = "";
-        answerContainer.innerHTML = userAnswer.join("");
+        answerContainer.innerHTML = game.userAnswer.join("");
     },
     drawHangMan: function() {
         let hangman = document.getElementById("hangman");
         let hangmanArr = ["hangman-0.jpg", "hangman-1.jpg", "hangman-2.jpg", "hangman-3.jpg", "hangman-4.jpg", "hangman-5.jpg", "hangman-6.jpg"];
         
-        hangIndex < 6 ? hangIndex++ : view.gameOver();
-        hangman.src = "img/" + hangmanArr[hangIndex];
+        if(view.hangIndex < 5) {
+            view.hangIndex++;
+            hangman.src = "img/" + hangmanArr[view.hangIndex];
+        } 
+        else {
+            view.hangIndex++;
+            hangman.src = "img/" + hangmanArr[view.hangIndex];
+            view.gameOver();
+        }        
     },
     gameOver: function() {
         let gameOverContainer = document.getElementById("game-over");
+        let overlay = document.getElementById("overlay");
+
         gameOverContainer.classList.toggle("hide");
+        overlay.classList.toggle("hide");
     }
 }
 
 game.getAnswerLength();
-alphabet.forEach((letter) => letter.addEventListener("click", game.userGuess));
+game.alphabet.forEach((letter) => letter.addEventListener("click", game.userGuess));
